54a2929cad8702fca57f3dff5c0518da971ae53c
Hameed-Ur-Rehman25 <hameedkhan944873@gmail.com>
2025-05-27 04:33:58 +0500

    del

--- android/app/src/main/kotlin/com/example/routivise/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.routivise
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:routivise/app/app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
